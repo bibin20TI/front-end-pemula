aa552238f3fc14bd5ff710067f7a0259775c11fa
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-09-22 11:38:15 +0700

    inisiasi awal

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+  const submitForm = document.getElementById("form");
+
+  submitForm.addEventListener("submit", function(event){
+
+       event.preventDefault();
+       addTodo();
+  });
+
+});
+
+// ini sangat susah sekali sehigga saya merasa frustasi sekali dengan apa yang saya alami
+// dengan demikian ini sangat susah sekali dengan mengerjakan hal seperti itu 
+// karena saya bukan programer sungguhan karna itu saya sangat kesulitan sekali menggerjakan projek ini 
